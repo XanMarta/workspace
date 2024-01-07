bad6fb8916f7e0a8a0b3217741e67a30e904d5bb
User <email@example.com>
2024-01-07 11:58:08 +0700

    Fix

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repository contains all neccesary tools to build my workspace in any environment. Those tools are Neovim, Font, Zsh configuration, etc. 
 
 
-## Lazyvim
+### Lazyvim
 
 ```bash
 curl -o- https://raw.githubusercontent.com/XanMarta/workspace/master/lazyvim/install.sh | bash
--- lazyvim/install.sh
+++ lazyvim/install.sh
@@ -3,19 +3,23 @@
 GITHUB_WORKSPACE=https://github.com/XanMarta/workspace.git
 
 # Clone workspace
+echo "----- Cloning workspace -----"
 mkdir -p ~/.temp
 rm -rf ~/.temp/workspace
 git clone $GITHUB_WORKSPACE ~/.temp/workspace
 
 # Backup old configuration
+echo "----- Backing up old configuration -----"
 if [ -d ~/.config/nvim ]; then
   rm -rf ~/.config/nvim.bk/
   mv ~/.config/nvim ~/.config/nvim.bk
 fi
 
 # Copy new configuration
+echo "----- Copying new configuration files -----"
 cp ~/.temp/workspace/lazyvim/nvim ~/.config/nvim -r
 
 # Remove workspace
+echo "----- Removing workspace -----"
 rm -rf ~/.temp/workspace
 
